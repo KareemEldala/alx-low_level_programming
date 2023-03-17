d667b48f1fb44484a0a57fbbc700892c3dd18f76
Kareem Reda <kareemeldala254@gmail.com>
2023-03-16 19:09:18 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * description : a c program that prints with put function
+ * Description: A C program that prints with printid function.
  *
- * return: always 0 (success)
- */
+ * Return: Always 0 (success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
