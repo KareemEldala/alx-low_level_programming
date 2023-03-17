df856e4d1c952884bda649052371463a1a9c0655
Kareem Reda <kareemeldala254@gmail.com>
2023-03-17 12:35:30 -0700

    a

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the alphapet
+ * main - print the upper and lower cases alphapet
  *
  * Description: A C program that prints alphapet.
  *
@@ -13,6 +13,9 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
 	putchar(c);
 	putchar('\n');
 	return (0);
