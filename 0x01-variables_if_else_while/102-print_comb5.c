687c45e3f08b7e7f8d3033725214a4d5b1d1387e
Kareem Reda <kareemeldala254@gmail.com>
2023-03-18 13:24:56 -0700

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printid function.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
+	{
+	for (q = p + 1; q <= 99; q++)
+	{
+	putchar((p / 10) + '0');
+	putcahr((p % 10) + '0');
+	putchar(' ');
+	putcahr((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (p == 98 && q == 99)
+        continue;
+	putchar(',');
+	putchar(' ');
+        }
+        }
+	putchar('\n');
+	return (0);
+}
+                                                   1,14          Top
