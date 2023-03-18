504c5b2d51ce07f5a0fca8dc6930a284d3da0d2c
Kareem Reda <kareemeldala254@gmail.com>
2023-03-18 11:01:34 -0700

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printid function.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n =  'z'; n >= 'a'; n--)
+	putchar(n);
+	puitchar('\n');
+
+	return (0);
+}
