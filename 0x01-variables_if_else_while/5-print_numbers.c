834b7c5d70dfce8cd32cb9d255220cbe682d04cb
Kareem Reda <kareemeldala254@gmail.com>
2023-03-17 22:00:17 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printid function.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	for(n, n < 10, n++)
+	{
+	printf(n);
+	}
+	return(0);
+}
