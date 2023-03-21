0c196c9b7d3b0c891451fb46472ab7617ec42ea3
Kareem Reda <kareemeldala254@gmail.com>
2023-03-21 16:22:21 -0700

    a

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+/**
+ *print_last_digit -  if n is greater than zero,
+ *          0 if n is zero and - if n is less
+ *          than 0
+ *
+ *@n: takes input from other function.
+ *
+ * Return: 1 if +, 0 if 0 and -1 if
+ *
+*/
+
+int print_last_digit(int n);
+{
+	int lasDigit;
+
+	if (n < 0)
+		lasDigit = -1 * (n % 10);
+	else
+		lasDigit = n % 10;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
